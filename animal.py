f26353f983550b7dc6687e4460785d0dfdde68ee
Daniel Martínez <danieljmp07@gmail.com>
2022-06-24 01:14:32 -0400

    added weight and height attributes

--- animal.py
+++ animal.py
@@ -1,5 +1,5 @@
 class Animal:
-    def __init__(self, species: str, name: str, age: int, illness: str):
+    def __init__(self, species: str, name: str, age: int, weight: float, height: float, illness: str):
 
         assert type(species) == str, f"Species should be a string"
         assert species.strip() != "", f"Species must be provided"
@@ -7,12 +7,18 @@ class Animal:
         assert name.strip() != "", f"A name must be provided"
         assert type(age) == int, f"Age should be a integer"
         assert age > 0, f"Age must be greater than 0"
+        assert type(weight) == float, f"Weight must be a number"
+        assert weight > 0, f"Weight must be greater than 0"
+        assert type(height) == float, f"Height must be a number"
+        assert height > 0; f"Height must be greater than 0"
         assert type(illness) == str, f"Illness should be a string"
         assert illness.strip() != "", f"Illness must be provided"
 
         self.__species = species
         self.__name = name
         self.__age = age
+        self.__weight = weight
+        self.__height = height
         self.__illness = illness
 
     @property
@@ -26,6 +32,14 @@ class Animal:
     @property
     def age(self):
         return self.__age
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @property
+    def height(self):
+        return self.__height
 
     @property
     def illness(self):
